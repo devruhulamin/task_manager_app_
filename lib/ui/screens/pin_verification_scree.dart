e81686333e79352ba95401c0ec4ca75b32d639d8
devruhulamin <pythonruhul@gmail.com>
2023-11-08 21:12:16 +0600

    pin verification done

---
+++ lib/ui/screens/pin_verification_scree.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:task_manager_app/ui/screens/login_screen.dart';
+import 'package:task_manager_app/ui/widgets/default_background.dart';
+
+class PinVerificationScreen extends StatelessWidget {
+  const PinVerificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: DefaultBackGround(
+          child: SafeArea(
+            child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 200, left: 50, right: 20, bottom: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pin Verification",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "A 6 digit verification pin will send to your email address.",
+                          style: TextStyle(color: Colors.grey, fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: Pinput(
+                            defaultPinTheme: defaultPinTheme,
+                            focusedPinTheme: focusedPinTheme,
+                            submittedPinTheme: submittedPinTheme,
+                            validator: (s) {
+                              return s == '2222' ? null : 'Pin is incorrect';
+                            },
+                            pinputAutovalidateMode:
+                                PinputAutovalidateMode.onSubmit,
+                            showCursor: true,
+                            onCompleted: (pin) => print(pin),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Verify",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Center(
+                            child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Have An Account?",
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginScreen(),
+                                      ),
+                                      (route) => false);
+                                },
+                                child: const Text(
+                                  "Signin",
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                          ],
+                        )),
+                      ]),
+                )),
+          ),
+        ));
+  }
+}
+
+final defaultPinTheme = PinTheme(
+  width: 56,
+  height: 56,
+  textStyle: const TextStyle(
+      fontSize: 20,
+      color: Color.fromRGBO(30, 60, 87, 1),
+      fontWeight: FontWeight.w600),
+  decoration: BoxDecoration(
+    border: Border.all(color: Colors.grey),
+    borderRadius: BorderRadius.circular(10),
+  ),
+);
+
+final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+  border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+  borderRadius: BorderRadius.circular(8),
+);
+
+final submittedPinTheme = defaultPinTheme.copyWith(
+  decoration: defaultPinTheme.decoration?.copyWith(
+    color: Color.fromRGBO(234, 239, 243, 1),
+  ),
+);
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:task_manager_app/ui/screens/forgot_password_screen.dart';
 import 'package:task_manager_app/ui/screens/home_screen.dart';
 import 'package:task_manager_app/ui/screens/login_screen.dart';
+import 'package:task_manager_app/ui/screens/pin_verification_scree.dart';
 import 'package:task_manager_app/ui/widgets/default_background.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -40,7 +41,7 @@ void goToHome(BuildContext context) {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => const ForgotPasswordScreen(),
+            builder: (context) => const PinVerificationScreen(),
           ));
     },
   );
