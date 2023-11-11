2dae5be63c840d96f069657b1f811d65ee15dabf
devruhulamin <pythonruhul@gmail.com>
2023-11-11 12:23:46 +0600

    update profile screen design

---
+++ lib/ui/screens/update_profile_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_app/ui/widgets/default_background.dart';
+import 'package:task_manager_app/ui/widgets/profile_bar.dart';
+
+class UpdateProfileScreen extends StatelessWidget {
+  const UpdateProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: DefaultBackGround(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const ProfileBar(),
+              const SizedBox(height: 50),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Update Profile",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            // width: 50,
+                            alignment: Alignment.center,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[700],
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10)),
+                            ),
+                            child: const Text(
+                              "Photos",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 2,
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 10),
+                            // width: 90,
+                            alignment: Alignment.centerLeft,
+                            height: 35,
+                            color: Colors.white,
+                            child: const Text("image1.jpeg"),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Email: ',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'First Name: ',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Last name: ',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Phone: ',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Password: ',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.arrow_circle_right_outlined,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
--- lib/ui/widgets/profile_bar.dart
+++ lib/ui/widgets/profile_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/ui/screens/update_profile_screen.dart';
 
 class ProfileBar extends StatelessWidget {
   const ProfileBar({super.key});
@@ -7,20 +8,27 @@ class ProfileBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(color: Colors.green),
-      child: const ListTile(
+      child: ListTile(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const UpdateProfileScreen(),
+              ));
+        },
         tileColor: Colors.green,
-        leading: CircleAvatar(
+        leading: const CircleAvatar(
           child: Icon(Icons.person),
         ),
-        title: Text(
+        title: const Text(
           "Rabbil Hossen",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
         ),
-        subtitle: Text(
+        subtitle: const Text(
           "rabbil@gmail.com",
           style: TextStyle(color: Colors.white),
         ),
-        trailing: Icon(
+        trailing: const Icon(
           Icons.arrow_forward,
           color: Colors.white,
         ),
